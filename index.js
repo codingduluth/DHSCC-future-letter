f10f6b989220da53ee691a35b874c040c8be3857
Uzziel Gonzalez <uzzielgonzalez247@gmail.com>
2024-08-28 19:20:02 -0400

    Version 2.1.2 (Bug Fixes)

--- index.js
+++ index.js
@@ -29,6 +29,23 @@ app.use(helmet());
 app.use(cors());
 app.use(xss());
 
+const allowedOrigins = [
+  'http://localhost:3000',
+  'https://dhscc-future-letter.onrender.com',
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+  })
+);
+
 //Routers
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/letters', authenticateUser, letterRouter);
